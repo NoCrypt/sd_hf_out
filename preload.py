02cfb80ac60bc63bc199f958a6ed3b2eec7aecf7
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2023-09-04 07:11:32 +0700

    fix 2

--- preload.py
+++ preload.py
@@ -3,4 +3,4 @@ from modules import paths
 
 
 def preload(parser):
-    parser.add_argument("--hf-token-out", type=int, help="HF Token for HF Out")
+    parser.add_argument("--hf-token-out", type=str, help="HF Token for HF Out")
